783d9c7341de47d3e8c6c08a8a2bb9ef5f29175a
Akash <jadhavhakash1210@gmail.com>
2023-04-23 10:16:16 +0530

    Added arryalist code

---
+++ akash/src/git/demo.java
@@ -0,0 +1,16 @@
+package git;
+
+import java.util.ArrayList;
+
+public class demo {
+	
+		public static void main(String[] args) {
+			ArrayList<Integer> a1=new ArrayList<>();
+			a1.add(12);
+			a1.add(20);
+			a1.add(25);
+			System.out.println(a1);
+			
+		
+	}
+}
