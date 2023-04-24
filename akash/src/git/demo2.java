4ffd1b2d368ef75a52298364487303272943f0b2
Akash <jadhavhakash1210@gmail.com>
2023-04-24 08:43:07 +0530

    created demo2 added code for set

---
+++ akash/src/git/demo2.java
@@ -0,0 +1,15 @@
+package git;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class demo2 {
+	public static void main(String[] args) {
+		Set<Integer> s=new HashSet<>();
+		s.add(12);
+		s.add(27);
+		System.out.println(s);
+	}
+
+}
