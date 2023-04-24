9f845dc9831287b8bbb3afb0f43e7c8fc1849e2b
Akash <jadhavhakash1210@gmail.com>
2023-04-24 08:11:21 +0530

    third version added element to arraylist

--- akash/src/git/demo.java
+++ akash/src/git/demo.java
@@ -7,8 +7,6 @@ public class demo {
 		public static void main(String[] args) {
 			ArrayList<Integer> a1=new ArrayList<>();
 			a1.add(12);
-			a1.add(20);
-			a1.add(25);
 			a1.add(31);
 			System.out.println(a1);
 			
